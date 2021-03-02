2435cc13ec45cbd749a69cb4b5cd6acbb69cb5a0
Kristian <kristian.stensgard96@gmail.com>
2021-03-02 14:12:23 +0100

    Finished task4. task3 needs more work

---
+++ Assignments/Assignment_3/code/task4a.py
@@ -0,0 +1,88 @@
+from torchvision import transforms, datasets, models
+import torch
+import utils
+import typing
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+
+from trainer import Trainer
+import numpy as np
+
+
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+
+def load_cifar10(batch_size: int, validation_fraction: float = 0.1) -> typing.List[DataLoader]:
+    # Note that transform train will apply the same transform for
+    # validation!
+    transform_train = transforms.Compose([
+        transforms.Resize([224, 224]),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.Resize([224, 224]),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+
+    data_train = datasets.CIFAR10('data/cifar10',
+                                  train=True,
+                                  download=True,
+                                  transform=transform_train)
+
+    data_test = datasets.CIFAR10('data/cifar10',
+                                 train=False,
+                                 download=True,
+                                 transform=transform_test)
+
+    indices = list(range(len(data_train)))
+    split_idx = int(np.floor(validation_fraction * len(data_train)))
+
+    val_indices = np.random.choice(indices, size=split_idx, replace=False)
+    train_indices = list(set(indices) - set(val_indices))
+
+    train_sampler = SubsetRandomSampler(train_indices)
+    validation_sampler = SubsetRandomSampler(val_indices)
+
+    dataloader_train = DataLoader(data_train,
+                                  sampler=train_sampler,
+                                  batch_size=batch_size,
+                                  num_workers=2,
+                                  drop_last=True)
+
+    dataloader_val = DataLoader(data_train,
+                                sampler=validation_sampler,
+                                batch_size=batch_size,
+                                num_workers=2)
+
+    dataloader_test = DataLoader(data_test,
+                                 batch_size=batch_size,
+                                 shuffle=False,
+                                 num_workers=2)
+
+    return dataloader_train, dataloader_val, dataloader_test
+
+
+if __name__ == "__main__":
+    # Set the random generator seed (parameters, shuffling etc).
+    # You can try to change this and check if you still get the same result!
+    utils.set_seed(0)
+    epochs = 5
+    batch_size = 32
+    learning_rate = 5e-4
+    early_stop_count = 4
+    dataloaders = load_cifar10(batch_size)
+    model = models.resnet18(pretrained=True)
+    trainer = Trainer(
+        batch_size,
+        learning_rate,
+        early_stop_count,
+        epochs,
+        model,
+        dataloaders
+    )
+    trainer.train()
+    create_plots(trainer, "task4a")
--- Assignments/Assignment_3/code/trainer.py
+++ Assignments/Assignment_3/code/trainer.py
@@ -1,4 +1,3 @@
-
 import torch
 import typing
 import time
